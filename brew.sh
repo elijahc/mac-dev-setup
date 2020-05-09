658960715fd49e6c80f1c93908c35c099dc90118
Elijah Christensen <ejd.christensen@gmail.com>
2020-05-09 15:20:49 -0600

    added brew.sh

---
+++ brew.sh
@@ -0,0 +1,24 @@
+brew update
+
+# Install homebrew core utils
+brew install \
+	git \
+	make \
+	grep \
+	tree \
+	readline \
+	wget \
+	curl \
+	xz \
+	zlib \
+	zsh
+
+# Cask
+brew cask install \
+  visual-studio-code \
+  google-chrome \
+  firefox \
+  spotify \
+  iterm2 \
+  slack \
+  vlc \
